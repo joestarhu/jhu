87bb27e338178763aba196dc4e1eb1a401bcffee
joestarhu <joestarhu@163.com>
2022-06-10 22:58:15 +0800

    更新代码细节

--- webhook/wecom.py
+++ webhook/wecom.py
@@ -8,8 +8,10 @@ from enum import Enum
 from typing import Optional
 from requests import Session
 
+
 class WeComMsgType(str, Enum):
     txt = 'text'
+
 
 class BaseContent:
     def __init__(self, **kw):
@@ -23,12 +25,15 @@ class BaseContent:
     def data(self, data):
         self._data = data
 
+
 class WeComText(BaseContent):
     def __init__(self, content: str, mentioned_list: Optional[list] = None, mentioned_mobile_list: Optional[list] = None):
         if len(content) > 2048:
             raise Exception('content内容长度不能超过2048字节')
         super().__init__(msgtype=WeComMsgType.txt.value, content=content,
                          mentioned_list=mentioned_list, mentioned_mobile_list=mentioned_mobile_list)
+
+
 class WeComHook:
     def __init__(self, url: str):
         self.url = url
@@ -39,10 +44,8 @@ class WeComHook:
         rsp.encoding = rsp.apparent_encoding
         return rsp
 
+
 if __name__ == '__main__':
-    ...
-    # """
-    # wh = WeComHook('******')
-    # t = WeComText("大家好,请", ['@all'])
-    # wh.send(t)
-    # """
+    wh = WeComHook('******')
+    t = WeComText("大家好,请", ['@all'])
+    wh.send(t)
