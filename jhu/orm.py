da6414ab43c3fc1009e16f3ea6aaea83cc19059b
joestarhu <joestarhu@163.com>
2024-07-22 13:52:18 +0800

    ORM update

--- jhu/orm.py
+++ jhu/orm.py
@@ -4,8 +4,7 @@ from math import ceil
 from typing import Callable, Any
 from urllib.parse import quote_plus
 
-from sqlalchemy import BigInteger, DateTime, func, select, Select, MappingResult
-from sqlalchemy.orm import DeclarativeBase, Mapped as M, mapped_column as mc
+from sqlalchemy import func, select, Select, MappingResult
 from sqlalchemy.orm.session import Session
 from sqlalchemy.sql.elements import BinaryExpression
 
@@ -22,14 +21,6 @@ class ORMFormatRule:
     """格式化"""
     filed: str
     format: Callable
-
-
-class ModelBase(DeclarativeBase):
-    id: M[int] = mc(BigInteger, primary_key=True, comment="ID")
-    create_dt: M[datetime] = mc(DateTime, comment="数据创建时间")
-    update_dt: M[datetime] = mc(DateTime, comment="数据更新时间")
-    create_id: M[int] = mc(BigInteger, nullable=True, comment="数据创建者账户ID")
-    update_id: M[int] = mc(BigInteger, nullable=True, comment="数据更新者账户ID")
 
 
 def format_datetime(dt: datetime) -> str:
